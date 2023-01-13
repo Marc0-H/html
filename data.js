3bc747739bc9d0d593b9d2d468d0f75c45675ccc
rcg270 <robbychingjong@gmail.com>
2023-01-13 09:18:36 +0100

    data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+var threads = [
+  {
+      title: "First thread",
+      author: "robby",
+      dat: Date.now(),
+      content: "Thread content",
+      comments: [
+        {
+          author: "Anonymous",
+          date: Date.now(),
+          content: "Hello"
+        },
+        {
+          author: "Anonymous2",
+          date: Date.now(),
+          content: "hello!!"
+        }
+      ]
+  },
+  {
+    title: "Thread 2",
+    author: "robby",
+    dat: Date.now(),
+    content: "Thread content",
+    comments: [
+      {
+        author: "Anonymous",
+        date: Date.now(),
+        content: "Hello"
+      },
+      {
+        author: "Anonymous2",
+        date: Date.now(),
+        content: "hello!!"
+      }
+    ]
+}
+]
