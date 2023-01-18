0ff9899f5f5306c7882ffe810f3a7722e6529856
rcg270 <robbychingjong@gmail.com>
2023-01-18 22:05:51 +0100

    minimize-button fix

--- minimize.js
+++ minimize.js
@@ -1,2 +1,9 @@
-document.getElementById('thread-minimize-button').addEventListener("click", )
-
+document.getElementById("thread-minimize-button").addEventListener("click", function(){
+  console.log("yeaboi");
+  var image = document.getElementById("thread-image");
+  if (image.style.display == "none") {
+    image.style.display = "block";
+  } else {
+    image.style.display = "none";
+  }
+});
