3c12d7859329809184af1bb8e5e84fe38eaa57f3
rcg270 <robbychingjong@gmail.com>
2023-01-22 13:50:24 +0100

    newthread.php

--- newthread.php
+++ newthread.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>newpost</title>
+  <title>New thread</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <script src="main.js" defer></script>
@@ -15,35 +15,32 @@
 <body>
   <header>
     <div class="inner__header">
-
-        <div class="logo__container">
-            <h1 class="logo">LOGO</h1>
+      <div class="logo__container">
+          <h1 class="logo">LOGO</h1>
+      </div>
+      <div class="header-content-right">
+        <div class="search-bar__container">
+          <input class="search-bar" type="text" placeholder="Search topic..">
         </div>
-
-        <div class="header-content-right">
-            <div class="search-bar__container">
-                <input class="search-bar" type="text" placeholder="Search topic..">
-            </div>
-            <div class="notification-button__container">
-                <button class="notification-button">
-                    <svg data-name="Layer 1" id="Layer_1" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><title/><path d="M40.62,28.34l-.87-.7A2,2,0,0,1,39,26.08V18A15,15,0,0,0,26.91,3.29a3,3,0,0,0-5.81,0A15,15,0,0,0,9,18v8.08a2,2,0,0,1-.75,1.56l-.87.7a9,9,0,0,0-3.38,7V37a4,4,0,0,0,4,4h8.26a8,8,0,0,0,15.47,0H40a4,4,0,0,0,4-4V35.36A9,9,0,0,0,40.62,28.34ZM24,43a4,4,0,0,1-3.44-2h6.89A4,4,0,0,1,24,43Zm16-6H8V35.36a5,5,0,0,1,1.88-3.9l.87-.7A6,6,0,0,0,13,26.08V18a11,11,0,0,1,22,0v8.08a6,6,0,0,0,2.25,4.69l.87.7A5,5,0,0,1,40,35.36Z"/></svg>
-                </button>
-            </div>
-            <div class="action-button__container">
-                <button id="action-button" class="action-button">
-                    <svg id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M28,14H18V4c0-1.104-0.896-2-2-2s-2,0.896-2,2v10H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h10v10c0,1.104,0.896,2,2,2  s2-0.896,2-2V18h10c1.104,0,2-0.896,2-2S29.104,14,28,14z"/></svg>
-                </button>
-                <div class="action-options">
-                    <button class="create-post-btn">Create post</button>
-                    <button class="start-chat-btn">Start chat</button>
-                </div>
-            </div>
-
-            <div class="profile__container">
-                <img src="./images/profile.jpg" alt="profile" class="profile">
-                <div class="online_status"></div>
-            </div>
+        <div class="notification-button__container">
+          <button class="notification-button">
+            <svg data-name="Layer 1" id="Layer_1" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><title/><path d="M40.62,28.34l-.87-.7A2,2,0,0,1,39,26.08V18A15,15,0,0,0,26.91,3.29a3,3,0,0,0-5.81,0A15,15,0,0,0,9,18v8.08a2,2,0,0,1-.75,1.56l-.87.7a9,9,0,0,0-3.38,7V37a4,4,0,0,0,4,4h8.26a8,8,0,0,0,15.47,0H40a4,4,0,0,0,4-4V35.36A9,9,0,0,0,40.62,28.34ZM24,43a4,4,0,0,1-3.44-2h6.89A4,4,0,0,1,24,43Zm16-6H8V35.36a5,5,0,0,1,1.88-3.9l.87-.7A6,6,0,0,0,13,26.08V18a11,11,0,0,1,22,0v8.08a6,6,0,0,0,2.25,4.69l.87.7A5,5,0,0,1,40,35.36Z"/></svg>
+          </button>
         </div>
+        <div class="action-button__container">
+          <button id="action-button" class="action-button">
+            <svg id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M28,14H18V4c0-1.104-0.896-2-2-2s-2,0.896-2,2v10H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h10v10c0,1.104,0.896,2,2,2  s2-0.896,2-2V18h10c1.104,0,2-0.896,2-2S29.104,14,28,14z"/></svg>
+          </button>
+          <div class="action-options">
+            <button class="create-post-btn">Create post</button>
+            <button class="start-chat-btn">Start chat</button>
+          </div>
+        </div>
+        <div class="profile__container">
+          <img src="./images/profile.jpg" alt="profile" class="profile">
+          <div class="online_status"></div>
+        </div>
+      </div>
     </div>
   </header>
 
@@ -118,14 +115,14 @@
                 <textarea name="post_content" id="new_thread_textarea" form="new_thread_form" maxlength="100" placeholder="Enter text..." required></textarea>
                 <div class="new_thread_image_container">
                   <label for="new_thread_image">Add image:</label>
-                  <input id="new_thread_image" type="file" form="new_thread_form">
+                  <input id="new_thread_image" type="file" form="new_thread_form">  <!--add name !-->
                 </div>
                 <hr>
                 <div class="new_thread_bottom">
                   <div class="new_thread_tag">
                     <p class="tag_text">Tag:</p>
                     <div class="tag__container">
-                      <select id="tag_selector" form="new_thread_form">
+                      <select name="post_tag" id="tag_selector" form="new_thread_form">
                         <option value="math">Math</option>
                         <option value="biology">Biology</option>
                         <option value="english">English</option>
--- post_upload.php
+++ post_upload.php
@@ -1,8 +1,30 @@
+<?php
+// define variables and set to empty values
+$post_title = $post_content = $post_tag = "";
+// $post_id  = $post_image =
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $post_title = test_input($_POST["post_title"]);
+  $post_content = test_input($_POST["post_content"]);
+  $post_tag = test_input($_POST["post_tag"]);
+  // $post_id = test_input($_POST["post_id"]);
+  // $post_image = test_input($_POST["post_image"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
 <html>
 <body>
 
-Welcome title:<?php echo $_POST["post_title"]; ?><br>
-Post content: <?php echo $_POST["post_content"]; ?>
+Welcome tipoeperstle:<?php echo $_POST["post_title"]; ?><br>
+Post content: <?php echo $_POST["post_content"]; ?><br>
+tag: <?php echo $_POST["post_tag"]; ?>
 
 </body>
 </html>
