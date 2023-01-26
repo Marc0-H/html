b68dd792c728772c5e7afe107ab3ec396881ba4b
Max Boot <maxfboot@gmail.com>
2023-01-26 14:36:09 +0100

    Sidebar aangepast in index.php

--- index.php
+++ index.php
@@ -74,57 +74,57 @@ session_start();
                         <p>Tempora maxime similique cum iure architecto fuga libero labore ullam tenetur delectus deserunt, nihil porro est nesciunt magnam repellat qui quos!  <br><br> Tempora quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, saepe! <br><br> Made with ❤️ by the brainy-bunch.</p>
                     </div>
                     <div class="sidebar_filter">
-                        <button id="filter-option-subject" class="dropdown-button">
-                            Subject
-                            <svg class="dropdown-button-icon" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
-                        </button>
-                        <div class="dropdown" id="dropdown-subject">
-                            <label class="dropdown-label">
-                                <input type="checkbox">Math
-                            </label>
-                            <label  class="dropdown-label">
-                                <input type="checkbox">Biology
-                            </label>
-                            <label class="dropdown-label">
-                                <input type="checkbox">Chemistry
-                            </label>
-                        </div>
-                        <button id="filter-option-sortby" class="dropdown-button">
-                            Sort by
-                            <svg class="dropdown-button-icon" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
-                        </button>
-                        <div class="dropdown" id="dropdown-sortby">
-                            <label class="dropdown-label">
-                                <input type="radio" name="sortby">Newest
-                            </label>
-                            <label  class="dropdown-label">
-                                <input type="radio" name="sortby">Relevance
-                            </label>
-                            <label class="dropdown-label">
-                                <input type="radio" name="sortby">Likes
-                            </label>
-                            <label class="dropdown-label">
-                                <input type="radio" name="sortby">Answered
-                            </label>
-                        </div>
-                        <button id="filter-option-user-role" class="dropdown-button">
-                            User role
-                            <svg class="dropdown-button-icon" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
-                        </button>
-                        <div class="dropdown" id="dropdown-user-role">
-                            <label class="dropdown-label">
-                                <input type="checkbox">PhD.
-                            </label>
-                            <label  class="dropdown-label">
-                                <input type="checkbox">Student
-                            </label>
-                            <label class="dropdown-label">
-                                <input type="checkbox">Teacher
-                            </label>
-                        </div>
-                        <button class="filter-button">
-                            Apply
-                        </button>
+                        <form method="post">
+                            <button id="filter-option-subject" class="dropdown-button" type="button">
+                                Subject
+                                <svg class="dropdown-button-icon" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                            </button>
+                            <div class="dropdown" id="dropdown-subject">
+                                <label class="dropdown-label">
+                                    <input name="filter-subject[]" value="math" type="checkbox">Math
+                                </label>
+                                <label  class="dropdown-label">
+                                    <input name="filter-subject[]" value="biology" type="checkbox">Biology
+                                </label>
+                                <label class="dropdown-label">
+                                    <input name="filter-subject[]" value="english" type="checkbox">English
+                                </label>
+                            </div>
+                            <button id="filter-option-sortby" class="dropdown-button" type="button">
+                                Sort by
+                                <svg class="dropdown-button-icon" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                            </button>
+                            <div class="dropdown" id="dropdown-sortby">
+                                <label class="dropdown-label">
+                                    <input name="filter-sortby" value="latest" type="radio" >Latest
+                                </label>
+                                <label  class="dropdown-label">
+                                    <input name="filter-sortby" value="relevance" type="radio">Relevance
+                                </label>
+                                <label class="dropdown-label">
+                                    <input name="filter-sortby" value="likes" type="radio">Likes
+                                </label>
+                                <label class="dropdown-label">
+                                    <input name="filter-sortby" value="answered" type="radio">Answered
+                                </label>
+                            </div>
+                            <button id="filter-option-user-role" class="dropdown-button" type="button">
+                                User role
+                                <svg class="dropdown-button-icon" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                            </button>
+                            <div class="dropdown" id="dropdown-user-role">
+                                <label class="dropdown-label">
+                                    <input name="filter-role[]" value="phd" type="checkbox">PhD.
+                                </label>
+                                <label  class="dropdown-label">
+                                    <input name="filter-role[]" value="student" type="checkbox">Student
+                                </label>
+                                <label class="dropdown-label">
+                                    <input name="filter-role[]" value="teacher" type="checkbox">Teacher
+                                </label>
+                            </div>
+                            <input type="submit" value="Apply" class="filter-button">
+                        </form>
                     </div>
                 </div>
 
