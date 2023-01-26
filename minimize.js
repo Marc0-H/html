9959f877a6c026651bc119afbb90a1e775222e40
rcg270 <robbychingjong@gmail.com>
2023-01-26 15:55:46 +0100

    onzin files removed

--- minimize.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("thread-minimize-button").addEventListener("click", function(){
-  console.log("yeaboi");
-  var image = document.getElementById("thread-image");
-  if (image.style.display == "none") {
-    image.style.display = "block";
-  } else {
-    image.style.display = "none";
-  }
-});
