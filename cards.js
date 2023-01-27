dc515a748b7bc12f820f071f99906e34aedfc8c8
Marc0_H <marcellohermans@hotmail.nl>
2023-01-27 15:03:24 +0100

    bug fixes & btn fixes ig

--- cards.js
+++ cards.js
@@ -4,9 +4,16 @@ let allPosts = document.querySelectorAll('.post_container');
 
 allPosts.forEach(post => {
     post.addEventListener("click", () => {
-        let myURL = window.location.protocol + "//" + window.location.host + window.location.hostname;
+        // let myURL = window.location.protocol + "//" + window.location.host + window.location.hostname;
         let postLink = "/thread.php?v=" + post.id;
-        console.log(myURL + postLink);
-        window.location.href = postLink;
+        // console.log(myURL + postLink);
+        // window.location.href = postLink;
+        let newUrl = window.location.href;
+        if (newUrl.includes("index.php")) {
+            newUrl = newUrl.substring(0, newUrl.length - 9) + postLink;
+        } else {
+            newUrl = newUrl + postLink;
+        } 
+        window.location.href = newUrl;
     });
 });
