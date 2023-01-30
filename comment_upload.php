262064d7deae527479577370730162952ce2ccac
Fenner66 <maxfboot@gmail.com>
2023-01-30 16:22:03 +0100

    Post en comment likes werken

--- comment_upload.php
+++ comment_upload.php
@@ -15,6 +15,10 @@
         return $data;
     }
 
+    if (!isset($_SESSION["userId"])) {
+        header("Location: login_signup/login_page.php");
+    }
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $comment_content = test_input($_POST["comment_content"]);
         $post_id = test_input($_POST["post_id"]);
@@ -24,17 +28,16 @@
         if ($parent_comment_id == "") {
             $insert_comment_query = "INSERT INTO comments (post_id, comment_content, comment_datetime, user_id)
             VALUES ('$post_id', '$comment_content', '$comment_datetime', '$user_id')";
-        } else {
+        } else if ($post_id == "") {
             $insert_comment_query = "INSERT INTO comments (comment_content, comment_datetime, user_id, parent_comment_id)
             VALUES ('$comment_content', '$comment_datetime', '$user_id', '$parent_comment_id')";
         }
     }
 
     try {
+        $post_id = $_GET['v'];
         mysqli_query($connection, $insert_comment_query);
-        echo "<script>window.alert('Comment upload succes!');";
-        echo "window.location.href='index.php';";
-        echo "</script>";
+        header("Location: thread.php?v=$post_id");
     } catch (PDOExeption $e) {
         echo "ERROR!!!<br>";
         echo $insert_comment_query . "<br>" . $e->getMessage();
--- thread.php
+++ thread.php
@@ -11,6 +11,7 @@ session_start();
         <title>EduZone</title>
         <link rel="stylesheet" href="stylesheet.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
         <script src="filter.js" defer></script>
         <script src="header.js" defer></script>
         <script src="thread.js" defer></script>
@@ -29,68 +30,72 @@ session_start();
             <div class="main_container">
             <?php include 'sidebar.php'; ?>
                 <div class="thread_content_container">
+                <?php
+                    /* Check if post has parameter */
+                    if (!isset($_GET["v"])) {
+                        ?><p>Post could not be found</p><?php
+                    } else {
+                        $post_id = $_GET["v"];
+                        $post_query = "SELECT * FROM posts WHERE post_id = $post_id";
+                        $post_result = mysqli_fetch_assoc(mysqli_query($connection, $post_query));
 
-                    <?php
-                        if (isset($_GET["v"])) {
-                            $post_id = $_GET["v"];
-                            $post_query = "SELECT * FROM posts WHERE post_id = $post_id";
-                            $post_result = mysqli_query($connection, $post_query);
-                            $post_result = mysqli_fetch_assoc($post_result);
+                        $op_id = $post_result["user_id"];
+                        $op_query = "SELECT userUid, profile_image FROM users WHERE userId = $op_id";
+                        $op_result = mysqli_fetch_assoc(mysqli_query($connection, $op_query));
 
-                            $op_id = $post_result["user_id"];
-                            $op_query = "SELECT userUid, profile_image FROM users WHERE userId = $op_id";
-                            $op_result = mysqli_fetch_assoc(mysqli_query($connection, $op_query));
+                        $comment_count_query = "SELECT COUNT(post_id) FROM comments WHERE post_id = $post_id";
+                        $comment_count = mysqli_fetch_assoc(mysqli_query($connection, $comment_count_query));
 
-                            $comment_count_query = "SELECT COUNT(post_id) FROM comments WHERE post_id = $post_id";
-                            $comment_count = mysqli_fetch_assoc(mysqli_query($connection, $comment_count_query));
-                    ?>
+                        $like_count_query = "SELECT COUNT(post_id) FROM post_upvote_link WHERE post_id = $post_id";
+                        $like_count = mysqli_fetch_assoc(mysqli_query($connection, $like_count_query));
 
-                            <div class="post_container original_poster">
-                                <div class="post_title">
-                                    <span class="post_tag tag-<?php echo $post_result["post_tag"]?>">
-                                        <?php echo $post_result["post_tag"]?>
-                                    </span>
-                                    <?php echo $post_result["post_title"]?>
-                                </div>
-                                <div class="user_info_container">
-                                    <img src="images/default.png">
-                                    <div class="username"><?php echo $op_result["userUid"]?></div>
-                                    <div class="user_tag">PhD.</div>
-                                    <i class="material-icons">query_builder</i>
-                                    <div class="date"><?php echo $post_result["post_datetime"]?></div>
-                                </div>
+                        if (isset($_SESSION["userId"])) {
+                            $session_id = $_SESSION["userId"];
+                            $user_like_query = "SELECT COUNT(user_id) FROM post_upvote_link WHERE user_id = $session_id AND post_id = $post_id";
+                            $user_like = mysqli_fetch_assoc(mysqli_query($connection, $user_like_query));
+                        }
+                ?>
 
-                                <div class="post_image_container">
-                                    <?php
-                                        if (!empty($post_result["post_image"])) {
-                                    ?>
-                                            <img src="data:image/png;base64,<?php echo $post_result["post_image"]?>" alt="card1">
-                                    <?php
-                                        }
-                                    ?>
-                                </div>
-
-                                <div class="post_content"><?php echo $post_result["post_content"]?></div>
-                                <div id="bookmark"></div>
-                                <div class="interaction_container">
-                                    <i class="material-icons tooltip">thumb_up<div class="tooltip_text">Like</div></i>
-                                    <div class="post_like_count">12</div>
-                                    <a href="#bookmark" class="material-icons tooltip">forum<div class="tooltip_text">Go to replies</div></a>
-                                    <div class="post_comment_count"><?php echo $comment_count["COUNT(post_id)"]?></div>
-                                    <div id="reply-post" class="reply_button">Reply</div>
-                                </div>
-                                <form id="form-reply-post" action="comment_upload.php" method="post">
-                                    <textarea class="comment_box" name="comment_content" autocomplete="off" placeholder="Add a reply..." rows="1" required></textarea>
-                                    <input type="hidden" name="post_id" value="<?php echo $_GET["v"]?>">
-                                    <input type="hidden" name="parent_comment_id" value="">
-                                    <input class="submit_button" type="submit" value="Submit">
-                                </form>
+                        <div class="post_container original_poster">
+                            <div class="post_title">
+                                <span class="post_tag tag-<?php echo $post_result["post_tag"]?>">
+                                    <?php echo $post_result["post_tag"]?>
+                                </span>
+                                <?php echo $post_result["post_title"]?>
+                            </div>
+                            <div class="user_info_container">
+                                <img src="images/default.png">
+                                <div class="username"><?php echo $op_result["userUid"]?></div>
+                                <div class="user_tag">PhD.</div>
+                                <i class="material-icons">query_builder</i>
+                                <div class="date"><?php echo $post_result["post_datetime"]?></div>
                             </div>
 
-                            
-                    <?php
-                        }
-                    ?>
+                            <div class="post_image_container">
+                                <?php
+                                    if (!empty($post_result["post_image"])) {
+                                        ?><img src="data:image/png;base64,<?php echo $post_result["post_image"]?>" alt="card1"><?php
+                                    }
+                                ?>
+                            </div>
+
+                            <div class="post_content"><?php echo $post_result["post_content"]?></div>
+                            <div id="bookmark"></div>
+                            <div class="interaction_container">
+                                <i id="post-like-<?php echo $post_id ?>" class="material-icons tooltip like_button <?php if ($user_like["COUNT(user_id)"] > 0) { echo 'liked'; }?>">thumb_up<div class="tooltip_text">Like</div></i>
+                                <div class="like_count"><?php echo $like_count["COUNT(post_id)"]?></div>
+                                <a href="#bookmark" class="material-icons tooltip">forum<div class="tooltip_text">Go to replies</div></a>
+                                <div class="post_comment_count"><?php echo $comment_count["COUNT(post_id)"]?></div>
+                                <div id="reply-post" class="reply_button">Reply</div>
+                            </div>
+                            <form id="form-reply-post" action="comment_upload.php?v=<?php echo $_GET["v"]?>" method="post">
+                                <textarea class="comment_box" name="comment_content" autocomplete="off" placeholder="Add a reply..." rows="1" required></textarea>
+                                <input type="hidden" name="post_id" value="<?php echo $_GET["v"]?>">
+                                <input type="hidden" name="parent_comment_id" value="">
+                                <input class="submit_button" type="submit" value="Submit">
+                            </form>
+                        </div>
+
                     <!--
                     <div class="solution_container">
                         <div class="solution_sign">
@@ -100,7 +105,7 @@ session_start();
                         </div>
                         <div class="comment_container">
                             <div class="user_info_container">
-                                <img src="images/profile_img.png">
+                                <img src="images/default.png">
                                 <div class="username">Arthur</div>
                                 <div class="user_tag">King of the Britons</div>
                                 <i class="material-icons">query_builder</i>
@@ -111,7 +116,7 @@ session_start();
                             </div>
                             <div class="interaction_container">
                             <i class="material-icons tooltip">thumb_up<div class="tooltip_text">Like</div></i>
-                                <div class="post_like_count">12</div>
+                                <div class="like_count">12</div>
                                 <i class="material-icons tooltip">forum<div class="tooltip_text">Show replies</div></i>
                                 <div class="post_comment_count">4</div>
                                 <div class="reply_button">Reply</div>
@@ -131,11 +136,19 @@ session_start();
                             $comment_id = $comment_row["id"];
                             $subcomment_count_query = "SELECT COUNT(parent_comment_id) FROM comments WHERE parent_comment_id = $comment_id";
                             $subcomment_count = mysqli_fetch_assoc(mysqli_query($connection, $subcomment_count_query));
+
+                            $like_count_query = "SELECT COUNT(comment_id) FROM comment_upvote_link WHERE comment_id = $comment_id";
+                            $like_count = mysqli_fetch_assoc(mysqli_query($connection, $like_count_query));
+                            
+                            if (isset($_SESSION["userId"])) {
+                                $user_like_query = "SELECT COUNT(user_id) FROM comment_upvote_link WHERE user_id = $session_id AND comment_id = $comment_id";
+                                $user_like = mysqli_fetch_assoc(mysqli_query($connection, $user_like_query));
+                            }
                     ?>
 
                             <div class="comment_container <?php if ($user_id == $op_id) { echo 'original_poster'; }?>">
                                 <div class="user_info_container">
-                                    <img src="images/profile_img.png">
+                                    <img src="images/default.png">
                                     <div class="username"><?php echo $user_result["userUid"]?></div>
                                     <div class="user_tag">Leerling</div>
                                     <i class="material-icons">query_builder</i>
@@ -145,13 +158,13 @@ session_start();
                                     <p><?php echo $comment_row["comment_content"]?></p>
                                 </div>
                                 <div class="interaction_container">
-                                    <i class="material-icons tooltip">thumb_up<div class="tooltip_text">Like</div></i>
-                                    <div class="post_like_count">12</div>
-                                    <i id="hide-<?php echo $comment_id ?>" class="material-icons tooltip hide_replies">forum<div class="tooltip_text">Show replies</div></i>
+                                    <i id="comment-like-<?php echo $comment_id ?>" class="material-icons tooltip like_button <?php if ($user_like["COUNT(user_id)"] > 0) { echo 'liked'; }?>">thumb_up<div class="tooltip_text">Like</div></i>
+                                    <div class="like_count"><?php echo $like_count["COUNT(comment_id)"]?></div>
+                                    <i id="subcomment-<?php echo $comment_id ?>" class="material-icons tooltip hide_replies">forum<div class="tooltip_text">Show replies</div></i>
                                     <div class="post_comment_count"><?php echo $subcomment_count["COUNT(parent_comment_id)"]?></div>
                                     <div id="reply-<?php echo $comment_id ?>" class="reply_button">Reply</div>
                                 </div>
-                                <form id="form-reply-<?php echo $comment_id ?>" action="comment_upload.php" method="post">
+                                <form id="form-reply-<?php echo $comment_id ?>" action="comment_upload.php?v=<?php echo $_GET["v"]?>" method="post">
                                     <textarea class="comment_box" name="comment_content" autocomplete="off" placeholder="Add a reply..." rows="1" required></textarea>
                                     <input type="hidden" name="post_id" value="">
                                     <input type="hidden" name="parent_comment_id" value="<?php echo $comment_row["id"] ?>">
@@ -160,17 +173,27 @@ session_start();
                             </div>
 
                             <?php
-                                $subcomment_query = "SELECT * FROM comments WHERE parent_comment_id = $comment_id ORDER BY comment_datetime DESC";
+                                $subcomment_query = "SELECT * FROM comments WHERE parent_comment_id = $comment_id ORDER BY comment_datetime ASC";
                                 $subcomment_result = mysqli_query($connection, $subcomment_query);
 
                                 while($subcomment_row = mysqli_fetch_assoc($subcomment_result)) {
+                                    $subcomment_id = $subcomment_row['id'];
+
                                     $user_id = $subcomment_row['user_id'];
                                     $user_query = "SELECT userUid, profile_image FROM users WHERE userId = $user_id";
                                     $user_result = mysqli_fetch_assoc(mysqli_query($connection, $user_query));
+
+                                    $like_count_query = "SELECT COUNT(comment_id) FROM comment_upvote_link WHERE comment_id = $subcomment_id";
+                                    $like_count = mysqli_fetch_assoc(mysqli_query($connection, $like_count_query));
+        
+                                    if (isset($_SESSION["userId"])) {
+                                    $user_like_query = "SELECT COUNT(user_id) FROM comment_upvote_link WHERE user_id = $session_id AND comment_id = $subcomment_id";
+                                    $user_like = mysqli_fetch_assoc(mysqli_query($connection, $user_like_query));
+                                    }
                             ?>   
-                                    <div class="subcomment_container child-of-hide-<?php echo $comment_id ?> <?php if ($user_id == $op_id) { echo 'original_poster'; }?>">
+                                    <div class="subcomment_container subcomment-<?php echo $comment_id ?> <?php if ($user_id == $op_id) { echo 'original_poster'; }?>">
                                         <div class="user_info_container">
-                                            <img src="images/profile_img.png">
+                                            <img src="images/default.png">
                                             <div class="username"><?php echo $user_result["userUid"]?></div>
                                             <div class="user_tag">PhD.</div>
                                             <i class="material-icons">query_builder</i>
@@ -180,14 +203,15 @@ session_start();
                                             <p><?php echo $subcomment_row["comment_content"]?></p>
                                         </div>
                                         <div class="interaction_container">
-                                            <i class="material-icons tooltip">thumb_up<div class="tooltip_text">Like</div></i>
-                                            <div class="post_like_count">12</div>
+                                            <i id="comment-like-<?php echo $subcomment_id ?>" class="material-icons tooltip like_button <?php if ($user_like["COUNT(user_id)"] > 0) { echo 'liked'; }?>">thumb_up<div class="tooltip_text">Like</div></i>
+                                            <div class="like_count"><?php echo $like_count["COUNT(comment_id)"]?></div>
                                         </div>
                                     </div>
-                    <?php
+                <?php
                                 }
                         }
-                    ?>
+                    }
+                ?>
                 </div>
             </div>
         </main>
