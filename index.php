0887fa5a741407f5978eef873cb85f951965ebd5
Marc0_H <marcellohermans@hotmail.nl>
2023-01-31 15:13:41 +0100

    sidebar ajax fix

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ header('Cache-Control: max-age=900');
         <title>EduZone</title>
         <link rel="stylesheet" href="stylesheet.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" defer></script>
         <script src="filter.js" defer></script>
         <script src="masonry.js" defer></script>
         <script src="header.js" defer></script>
@@ -30,92 +31,71 @@ header('Cache-Control: max-age=900');
         ?>
         <main>
             <div class="main_container">
-              <?php include 'sidebar.php'; ?>
+
+                <div class="sidebar_container">
+                    <div class="sidebar_description">
+                        <h2>Find your Study Buddy</h2>
+                        <p>Welcome to our community! Here you can connect with fellow students and share knowledge and resources. Whether you need help with a specific subject or just want to discuss your coursework with others, our forum is the place to be. Join now and start building your support network!<br><br> Made with ❤️ by the brainy-bunch.</p>
+                    </div>
+                    <div class="sidebar_filter">
+                        <form class="filter_form" method="get">
+                            <button id="filter-option-subject" class="dropdown-button" type="button">
+                                Subject
+                                <svg class="dropdown-button-icon" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                            </button>
+                            <div class="dropdown" id="dropdown-subject">
+                                <label class="dropdown-label">
+                                    <input name="filter-subject[]" value="math" type="checkbox" checked>Math
+                                </label>
+                                <label  class="dropdown-label">
+                                    <input name="filter-subject[]" value="biology" type="checkbox" checked>Biology
+                                </label>
+                                <label class="dropdown-label">
+                                    <input name="filter-subject[]" value="english" type="checkbox" checked>English
+                                </label>
+                                <label class="dropdown-label">
+                                    <input name="filter-subject[]" value="general" type="checkbox" checked>General
+                                </label>
+                            </div>
+                            <button id="filter-option-sortby" class="dropdown-button" type="button">
+                                Sort by
+                                <svg class="dropdown-button-icon" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                            </button>
+                            <div class="dropdown" id="dropdown-sortby">
+                                <label class="dropdown-label">
+                                    <input name="filter-sortby" value="latest" type="radio" checked>Latest
+                                </label>
+                                <label class="dropdown-label">
+                                    <input name="filter-sortby" value="populairity" type="radio">Populairity
+                                </label>
+                                <label class="dropdown-label">
+                                    <input name="filter-sortby" value="controversial" type="radio">Controversial
+                                </label>
+                            </div>
+                            <button id="filter-option-user-role" class="dropdown-button" type="button">
+                                User role
+                                <svg class="dropdown-button-icon" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg"><title/><path d="M81.8457,25.3876a6.0239,6.0239,0,0,0-8.45.7676L48,56.6257l-25.396-30.47a5.999,5.999,0,1,0-9.2114,7.6879L43.3943,69.8452a5.9969,5.9969,0,0,0,9.2114,0L82.6074,33.8431A6.0076,6.0076,0,0,0,81.8457,25.3876Z"/></svg>
+                            </button>
+                            <div class="dropdown" id="dropdown-user-role">
+                                <label class="dropdown-label">
+                                    <input name="filter-role[]" value="phd" type="checkbox" checked>PhD.
+                                </label>
+                                <label  class="dropdown-label">
+                                    <input name="filter-role[]" value="student" type="checkbox" checked>Student
+                                </label>
+                                <label class="dropdown-label">
+                                    <input name="filter-role[]" value="teacher" type="checkbox" checked>Teacher
+                                </label>
+                            </div>
+                            <input type="submit" value="Apply" class="filter-button">
+                        </form>
+                    </div>
+                </div>
+
                 <div class="main_content_container">
                     <div class="main_content_title">The latest posts...</div>
                     <div class="main_content_posts">
-                        <?php
-                        $query = "SELECT * FROM posts";
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            if (isset($_POST["filter-subject"])) {
-                                $subject = array();
-                                foreach($_POST["filter-subject"] as $filter) {
-
-                                    if (strpos($filter, "math") !== false) {
-
-                                        $subject[] = "math";
-                                    }
-                                    if (strpos($filter, "english") !== false) {
-
-                                        $subject[] = "english";
-                                    }
-                                    if (strpos($filter, "biology") !== false) {
-
-                                        $subject[] = "biology";
-                                    }
-                                    if (strpos($filter, "general") !== false) {
-
-                                        $subject[] = "general";
-                                    }
-                                }
-
-                                if (!empty($subject)) {
-                                    $query .= " WHERE post_tag IN ('" . implode("','", $subject) . "')";
-                                }
-                                $result = mysqli_query($connection, $query);
-                            }
-
-                        }
-                        // $result .= "ORDER BY post_id DESC";
-                        $post_result = mysqli_query($connection, $query);
-
-
-                        // $post_sql = "SELECT post_id, post_title, post_content, post_tag, post_datetime, post_image from posts ORDER BY post_id DESC";
-                        // $post_result = mysqli_query($connection, $post_sql);
-
-                        while($row = mysqli_fetch_assoc($post_result)) {                        
-                          $user_id = $row['user_id'];
-                          $user_query = "SELECT userUid, profile_image from users where userId = $user_id";
-                          $user_result = mysqli_query($connection, $user_query);
-                          $user_row = mysqli_fetch_assoc($user_result);
-                        ?>
-
-                        <div id="<?php echo $row["post_id"]?>" class="post_container">
-                            <div class="post_image_container">
-                                <?php
-                                    if (!empty($row["post_image"])) {
-                                ?>
-                                        <img src="data:image/png;base64,<?php echo $row["post_image"]?>" alt="card1">
-                                <?php
-                                    }
-                                ?>
-
-                            </div>
-                            <div class="post_content_container">
-                                <div class="post_title">
-                                  <span class="post_tag tag-<?php echo $row["post_tag"]?>">
-                                    <?php echo $row["post_tag"]?></span> <?php echo $row["post_title"]?></div>
-                                <div class="user_info_container">
-                                    <img src="images/default.png">
-                                    <div class="username" title="<?php echo $user_row["userUid"]?>"><?php echo $user_row["userUid"]?></div>
-                                    <div class="user_tag">PhD.</div>
-                                    <!-- <i class="material-icons">query_builder</i> -->
-                                    <div class="date" title="<?php echo $row["post_datetime"]?>"><?php echo $row["post_datetime"]?></div>
-                                </div>
-                                <div class="post_content">
-                                    <p><?php echo $row["post_content"]?></p>
-                                </div>
-                                <div class="interaction_container">
-                                    <i class="material-icons">thumb_up</i>
-                                    <div class="like_count">12</div>
-                                    <i class="material-icons">forum</i>
-                                    <div class="comment_count">4</div>
-                                </div>
-                            </div>
-                        </div>
-                        <?php
-                            }
-                        ?>
+                        <?php include 'filtered_results.php'  ?>
                     </div>
                 </div>
             </div>
