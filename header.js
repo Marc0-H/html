da7cdebf1649ef46013f4a304cdaabd398a20a67
Marc0_H <marcellohermans@hotmail.nl>
2023-01-31 14:48:23 +0100

    filter mobile fix

--- header.js
+++ header.js
@@ -1,13 +1,20 @@
 let hamburger = document.querySelector('.hamburger_menu_btn');
 
 hamburger.addEventListener("click", () => {
-    console.log("click");
+    let headerContentRight = document.querySelector(".header_content_right");
     hamburger.classList.toggle("hamburger_active");
+    headerContentRight.classList.toggle("hamburger_active");
 });
 
 let profile_button = document.querySelector('.profile_button');
 
 profile_button.addEventListener("click", () => {
-    console.log("click");
     profile_button.classList.toggle("profile_button_active");
+});
+
+let sidebarContainer = document.querySelector(".sidebar_container");
+let mobileFilterBtn = document.querySelector(".mobile_filter_btn");
+
+mobileFilterBtn.addEventListener("click", () => {
+    sidebarContainer.classList.toggle("sidebar_active");
 });
