4402f9f85b71efbfbe44c64e051cd8442d93003f
Georgi Arushanov <14275139@uva.nl>
2023-02-01 14:12:01 +0100

    attempt 400 for bug fix

--- pwupdate/php_scripts/update.php
+++ pwupdate/php_scripts/update.php
@@ -19,15 +19,18 @@ if (empty($URLtoken) || empty($selector) || empty($password)) {
 else {
     if (ctype_xdigit($URLtoken) && ctype_xdigit($selector) === TRUE) {
         $current_time = date("U");
-        $stmt = mysqli_prepare($connection, "SELECT pwdToken, userId FROM `reset` WHERE pwdSelector = ? AND expirationDate <= $current_time");
+        $stmt = mysqli_prepare($connection, "SELECT pwdToken, userId FROM `reset` WHERE pwdSelector = ? AND expirationDate >= ?");
         if($stmt === FALSE) {
            header("location: ../update_page?error=stmtfailed");
         }
         else {
-            mysqli_stmt_bind_param($stmt, "s", $selector);
+            mysqli_stmt_bind_param($stmt, "si", $selector, $current_time);
             mysqli_stmt_execute($stmt);
 
-            $DBaccess = mysqli_fetch_assoc(mysqli_stmt_get_result($stmt));
+            $result = mysqli_stmt_get_result($stmt);
+
+            $DBaccess = mysqli_fetch_assoc($result);
+
             if (mysqli_num_rows($DBaccess) > 0 ) {
                 $DBtoken = $DBaccess['pwdToken'];
                 $userId = $DBaccess['userId'];
